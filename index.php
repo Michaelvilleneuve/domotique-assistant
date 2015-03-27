b26ece1bf09b1bdff3baaf50efebf8d2499940ae
VILLENEUVE <contact@michaelvilleneuve.fr>
2015-03-27 09:13:14 +0100

    Ajout mode débug

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+/* Mode débugage : Décommentez pour activer 
 error_reporting(E_ALL);
-ini_set('display_errors','On');
-require_once 'routeur.php'; ?>
+ini_set('display_errors','On');*/
+require_once 'routeur.php'; 
+?>
